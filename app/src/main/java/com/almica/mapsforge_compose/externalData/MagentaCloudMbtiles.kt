6134b3896b4a0f33aedda06df283388f581e314b
almica <alt.micha@gmail.com>
2026-09-11 10:47:25 +0200

    11sep2026 MagentaCloud shared links completed mvt and mbtiles

---
+++ app/src/main/java/com/almica/mapsforge_compose/externalData/MagentaCloudMbtiles.kt
@@ -0,0 +1,57 @@
+package com.almica.mapsforge_compose.externalData
+
+class MagentaCloudMbtiles {
+    companion object {
+        val phonemaps: Map<String, String> =
+            mapOf(
+                Pair("tile_518_388_10_Phonemaps.mbtiles", "https://magentacloud.de/public.php/dav/files/4kPHep8DecRnRPR"),
+                Pair("tile_518_389_10_Phonemaps.mbtiles", "https://magentacloud.de/public.php/dav/files/eN57qMDLc6PBsDw"),
+                Pair("tile_519_388_10_Phonemaps.mbtiles", "https://magentacloud.de/public.php/dav/files/bHzx6KAFGJ8MQ4Y"),
+                Pair("tile_519_389_10_Phonemaps.mbtiles", "https://magentacloud.de/public.php/dav/files/jgfYG32WzEFJaDS"),
+                Pair("tile_520_388_10_Phonemaps.mbtiles", "https://magentacloud.de/public.php/dav/files/YNZnEczroPHWE7C"),
+                Pair("tile_521_388_10_Phonemaps.mbtiles", "https://magentacloud.de/public.php/dav/files/HLfEerq7a3kH7PH"),
+                Pair("tile_521_389_10_Phonemaps.mbtiles", "https://magentacloud.de/public.php/dav/files/GHLaArz7n9A4LPr"),
+                Pair("tile_540_337_10_Phonemaps.mbtiles", "https://magentacloud.de/public.php/dav/files/SrA8zbas5CJWa9i"),
+                Pair("tile_540_338_10_Phonemaps.mbtiles", "https://magentacloud.de/public.php/dav/files/RmyPnDX5wa2aZzx"),
+                Pair("tile_541_336_10_Phonemaps.mbtiles", "https://magentacloud.de/public.php/dav/files/SY3ZySE4CCJznr7"),
+                Pair("tile_541_337_10_Phonemaps.mbtiles", "https://magentacloud.de/public.php/dav/files/78PGPdTnoM4yc6d"),
+                Pair("tile_541_338_10_Phonemaps.mbtiles", "https://magentacloud.de/public.php/dav/files/kiZTDdNnPwe8HDm"),
+                Pair("tile_541_339_10_Phonemaps.mbtiles", "https://magentacloud.de/public.php/dav/files/H7zJ48rZzw8QSgz"),
+                Pair("tile_541_340_10_Phonemaps.mbtiles", "https://magentacloud.de/public.php/dav/files/bqbNDZwPtYrA7k9"),
+                Pair("tile_541_341_10_Phonemaps.mbtiles", "https://magentacloud.de/public.php/dav/files/nM6w4Zi2z6Qn5f9"),
+                Pair("tile_542_338_10_Phonemaps.mbtiles", "https://magentacloud.de/public.php/dav/files/H9gNAWJPyqdQPyF"),
+                Pair("tile_550_326_10_Phonemaps.mbtiles", "https://magentacloud.de/public.php/dav/files/HXFwtrJzFPjS9xT"),
+                Pair("tile_550_327_10_Phonemaps.mbtiles", "https://magentacloud.de/public.php/dav/files/sZe9xTNsxpcSGCH"),
+                Pair("tile_551_327_10_Phonemaps.mbtiles", "https://magentacloud.de/public.php/dav/files/j8xGjDPAjw4rYJP")
+            )
+        val opentopomap: Map<String, String> =
+            mapOf(
+                Pair("tile_539_336_10_OpenTopo.mbtiles", "https://magentacloud.de/public.php/dav/files/p8nfdebnxafWNtY"),
+                Pair("tile_539_337_10_OpenTopo.mbtiles", "https://magentacloud.de/public.php/dav/files/ZMLo92zjcoxsGC7"),
+                Pair("tile_540_335_10_OpenTopo.mbtiles", "https://magentacloud.de/public.php/dav/files/pNp6dEBjRXPjBgN"),
+                Pair("tile_540_336_10_OpenTopo.mbtiles", "https://magentacloud.de/public.php/dav/files/XNk8QxC2EbgAmtd"),
+                Pair("tile_540_337_10_OpenTopo.mbtiles", "https://magentacloud.de/public.php/dav/files/f5PLC32t9fACYoT"),
+                Pair("tile_540_338_10_OpenTopo.mbtiles", "https://magentacloud.de/public.php/dav/files/pc2F6fLjPartY6i"),
+                Pair("tile_541_335_10_OpenTopo.mbtiles", "https://magentacloud.de/public.php/dav/files/MW9LQejbECQyoSx"),
+                Pair("tile_541_336_10_OpenTopo.mbtiles", "https://magentacloud.de/public.php/dav/files/PZ53BeeJAMA4qLH"),
+                Pair("tile_541_338_10_OpenTopo.mbtiles", "https://magentacloud.de/public.php/dav/files/LHHytPmYW6FgtQj"),
+                Pair("tile_541_339_10_OpenTopo.mbtiles", "https://magentacloud.de/public.php/dav/files/84ZfSz6fMemx6e2"),
+                Pair("tile_542_335_10_OpenTopo.mbtiles", "https://magentacloud.de/public.php/dav/files/yPxfpBHHJSgwi8D"),
+                Pair("tile_542_336_10_OpenTopo.mbtiles", "https://magentacloud.de/public.php/dav/files/zp8Ggg2AeHBLSDs"),
+                Pair("tile_542_337_10_OpenTopo.mbtiles", "https://magentacloud.de/public.php/dav/files/Q7mERocBqtkkxos"),
+                Pair("tile_542_338_10_OpenTopo.mbtiles", "https://magentacloud.de/public.php/dav/files/wS3oHcGaPERP5pr"),
+                Pair("tile_542_339_10_OpenTopo.mbtiles", "https://magentacloud.de/public.php/dav/files/zsHtBR6CF5jdg5y"),
+                Pair("tile_600_399_10_OpenTopo.mbtiles", "https://magentacloud.de/public.php/dav/files/qbkka4mfocqfGL5"),
+                Pair("tile_601_399_10_OpenTopo.mbtiles", "https://magentacloud.de/public.php/dav/files/rLy6YNB75GcibRM")
+                )
+        val outdoor: Map<String, String> =
+            mapOf(
+                Pair("tile_541_336_10_Outdoor.mbtiles", "https://magentacloud.de/public.php/dav/files/WtygRsd4Zm9eDif"),
+                Pair("tile_541_338_10_Outdoor.mbtiles", "https://magentacloud.de/public.php/dav/files/D3mXwjbD7WC9KgH")
+            )
+        val thunderforest: Map<String, String> =
+            mapOf(
+                Pair("tile_541_338_10_Thunderforest.mbtiles", "https://magentacloud.de/public.php/dav/files/txy4FfZHyN4oQjY")
+            )
+    }
+}
